491af5d75e72c3c226289acb05165a04124564b1
Максим <Maxim>
2022-10-17 11:20:23 +0300

    hangman 2-nd stage

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -2,3 +2,12 @@ import random
 
 print("HANGMAN")
 print("The game will be available soon.")
+word = ["python"]
+a = input("Guess the word :\n>")
+while True:
+    if a == word:
+        print("You survived!")
+        break
+    else:
+        print("You lost!")
+        break
