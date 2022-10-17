e7eca54e77a1578c850c5eabaf06a3d8b72a7b1e
Максим <Maxim>
2022-10-17 11:29:18 +0300

    hangman 4-th stage

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -4,6 +4,9 @@ print("HANGMAN")
 print("The game will be available soon.")
 words = ["python" , "java" , "php" , "html"]
 word = random.choice(words)
-a = input("Guess the word :\n>")
-while True:
-   if a == word:
+length = '-' * len(word)
+a = input("Guess the word :" + word[:3] + "-".join([''for _ in range (len(word)-3)]) + "-\n>")
+if a == word:
+    print("You survive!")
+else:
+    print("You lose!")
