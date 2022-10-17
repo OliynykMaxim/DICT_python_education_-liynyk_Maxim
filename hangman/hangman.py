2a25c085dcaa4de2931160d0dd494025e5ce138c
Максим <Maxim>
2022-10-17 11:49:14 +0300

    hangman 5-th stage

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -2,11 +2,22 @@ import random
 
 print("HANGMAN")
 print("The game will be available soon.")
-words = ["python" , "java" , "php" , "html"]
-word = random.choice(words)
-length = '-' * len(word)
-a = input("Guess the word :" + word[:3] + "-".join([''for _ in range (len(word)-3)]) + "-\n>")
-if a == word:
-    print("You survive!")
-else:
-    print("You lose!")
+my_list = ["html", "java", "javascript", "python"]
+my_word = random.choice(my_list)
+remember_letters = []
+all_letters = list(set(my_word))
+word = "".join([i if i in remember_letters else "_" for i in my_word])
+print(word)
+life = 8
+while life > 0:
+    letter = input("Input a letter:")
+    if letter in my_word and letter not in remember_letters:
+        remember_letters.append(letter)
+        all_letters.remove(letter)
+    else:
+        life -= 1
+    word = "".join([i if i in remember_letters else "_" for i in my_word])
+    print(word)
+    if len(all_letters) == 0:
+        break
+print("thanks for playing\nWe'll see how well you did in the next stage")
