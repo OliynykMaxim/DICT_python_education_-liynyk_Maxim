b92bc42a0ed08c19c29dae82ed06639118676e2a
Максим <Maxim>
2022-10-17 12:03:21 +0300

    hnagman 7-th stage

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -5,27 +5,30 @@ print("The game will be available soon.")
 my_list = ["html", "java", "php", "python"]
 my_word = random.choice(my_list)
 remember_letters = []
+input_letters = []
 all_letters = list(set(my_word))
 word = "".join([i if i in remember_letters else "_" for i in my_word])
 print(word)
 life = 8
 while life > 0:
     letter = input("Input a letter:")
-    if letter in my_word and letter not in remember_letters:
-        remember_letters.append(letter)
-        all_letters.remove(letter)
-    if letter not in my_word:
-        life -= 1
-        print("That letter doesn't appear in the word")
-    if letter in remember_letters:
+    if letter == letter.lower() and len(letter) == 1:
+        if letter in my_word and letter not in remember_letters and letter not in input_letters:
+            remember_letters.append(letter)
+            all_letters.remove(letter)
+        elif letter in remember_letters or letter in input_letters:
+            print("You've already guessed this letter")
+        else:
+            print("That letter doesn't appear in the word")
             life -= 1
-            print("No improvements")
-    word = "".join([i if i in remember_letters else "_" for i in my_word])
-    print(word)
-    if life == 0 and len(all_letters) != 0:
-        print("You lose!")
-    if len(all_letters) == 0:
-        print("You win!")
-        break
-
-print("thanks for playing\nWe'll see how well you did in the next stage")
+        word = "".join([i if i in remember_letters else "_" for i in my_word])
+        print(word)
+        if len(all_letters) == 0:
+            print("You guessed the word\nYou survived!")
+            break
+    elif letter != letter.lower():
+        print("Please enter a lowercase English letter")
+    elif len(letter) != 1:
+        print("You should input a single letter")
+if len(all_letters) > 0:
+    print("You lost!")
