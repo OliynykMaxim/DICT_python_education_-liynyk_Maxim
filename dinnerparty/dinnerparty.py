0a42eee5c365fbf3459a4e2d70d97dace9881a1e
Максим <Maxim>
2022-10-23 16:10:03 +0300

    dinnerparty 3-rd stage

--- dinnerparty/dinnerparty.py
+++ dinnerparty/dinnerparty.py
@@ -1,3 +1,6 @@
+import random
+y_or_n = {'yes': True, 'no': False}
+
 num = int(input("Enter the number of friends joining (including you):\n>"))
 if num < 1:
     print('No one is joining for the party')
@@ -8,7 +11,14 @@ for _ in range(num):
     name = input(">")
     payments[name] = 0
 price = int(input("Enter the total amount:\n>"))
+print('Do you want to use "Who is lucky?" feature\n Type Yes/No')
+lucky = y_or_n[input(">").lower()]
+lucky_name = ''
+if lucky:
+    lucky_name = random.choice(list(payments.keys()))
+    print(lucky_name, "is lucky today!")
+else:
+    print("No one is going to be lucky")
 a = round(price/num, 2)
 for name in payments:
     payments[name] = a
-print(payments)
